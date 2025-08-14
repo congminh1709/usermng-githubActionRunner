0e03aa1aa386f1c564ded55b9f2d3759f1578cb1
congminh1709 <congminh1709@gmail.com>
2025-08-14 16:21:45 +0700

    update

---
+++ tests/e2e.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest');
+const assert = require('assert');
+
+describe('Test public API', () => {
+  it('should return status 200', async () => {
+    try {
+      const res = await request('http://localhost:3000')
+        .get('/posts/1')
+        .set('Accept', 'application/json')
+        .expect(200)
+    } catch (e) {
+      // console.log(`err: ${e}`);
+      assert.equal(true, false, `this err happened: ${e}`);
+    }
+  });
+
+});
