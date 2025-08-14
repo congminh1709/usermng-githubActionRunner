e22ff65ff4563ea5deb2250b36d15ed2aa8183ff
congminh1709 <congminh1709@gmail.com>
2025-08-14 16:01:59 +0700

    update

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const PORT = 3000
 const VERSION = "1.1"
 
 app.get('/', (req, res) => {
+    console.log(`called api /`);
     res.status(200).json({
         meta: {
             code: 200,
-            message: `server ${VERSION} OK`,
+            message: `server version ${VERSION} returns OK`,
         }
     })
 })
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`RestAPI ${VERSION} is running at port ${process.env.PORT || PORT}`);
+    console.log(`RestAPI version ${VERSION} is running at port ${process.env.PORT || PORT}`);
 });
